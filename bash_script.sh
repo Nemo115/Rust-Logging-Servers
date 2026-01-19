0b19d891fe6223340dcf75439d6884310303637a
Clu <stickman5464@gmail.com>
2026-01-19 10:16:30 +0000

    cleaned up misc files

--- bash_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting Logging Service"
-echo "Running script..."
-#cd /home/bumblebee/rust/script_testing
-cd /home/leozl/Desktop/rust/privacy_lock
-cargo run
--- setup_testing.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-PATH=$PATH:/home/leozl/Desktop/rust/privacy_lock/target/release
-echo setup done
